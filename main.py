74fb62150548fc1c6d729748f5a62e5f3340f8bf
TheBarbaraIsTaken <szabo.barbara.noemi@gmail.com>
2023-10-22 12:11:29 +0200

    handle post

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from model import Predicter
 from website import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    sentences = """
-        The big black cat stared at the small dog. 
-        Jane watched her brother in the evenings.
-        Have you seen it?
-        I want a cat.
-        The new car was bought by me.
-    """
-
-    feature = ("cat", None)
-
-    model = Predicter(sentences)
-
-    # print(model.pred_sent(feature))
-
     app.run(debug=True)
 
 
