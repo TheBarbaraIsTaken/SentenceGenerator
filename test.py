0896b6f8b00ad1dfa6ce0368bb3520d843fe749b
tobia <t.hoppe-1@student.utwente.nl>
2023-11-03 16:38:11 +0100

    copied file

---
+++ test.py
@@ -0,0 +1,29 @@
+from model_parser import Predicter
+
+
+def load_corpus(file_path):
+
+    if file_path:
+        with open(file_path, 'r') as f:
+            sentences = f.read()
+    else:
+        sentences = """
+            I have a cat.
+            She has a dog.
+        """
+
+    return sentences
+
+if __name__ == "__main__":
+    # text = load_corpus("hp.txt")
+    conll_file = "../Data/UD_English-EWT/en_ewt-ud-test.conllu"
+    file = "hp.txt"
+    
+    model = Predicter(conll_file)  # Predicter("hp.txt", stem=False)
+    feature = ("it", None)
+    
+    
+    for i in range(10):
+        s = (model.pred_sent(feature))
+        print(s)
+
