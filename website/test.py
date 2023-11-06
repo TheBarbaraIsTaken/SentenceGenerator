34a3bf8456433aa8cd7a1acadd5d6cc606f04751
tobia <t.hoppe-1@student.utwente.nl>
2023-11-06 13:54:55 +0100

    corpora included

--- website/test.py
+++ website/test.py
@@ -16,7 +16,7 @@ def load_corpus(file_path):
 
 if __name__ == "__main__":
     # text = load_corpus("hp.txt")
-    conll_file = "../Data/UD_English-EWT/en_ewt-ud-test.conllu"
+    conll_file = "\Project\SentenceGenerator\Data\UD_English-EWT/Data/UD_English-EWT/en_ewt-ud-test.conllu"
     file = "hp.txt"
     
     model = Predicter(conll_file)  # Predicter("hp.txt", stem=False)
