92bee1a4fc3cb10ca45bdd61af6937659d27d453
tobia <t.hoppe-1@student.utwente.nl>
2023-11-06 14:23:34 +0100

    fixed directory issue

--- website/test.py
+++ website/test.py
@@ -1,5 +1,5 @@
 from model_parser import Predicter
-
+import os
 
 def load_corpus(file_path):
 
@@ -16,9 +16,9 @@ def load_corpus(file_path):
 
 if __name__ == "__main__":
     # text = load_corpus("hp.txt")
-    conll_file = "\Project\SentenceGenerator\Data\UD_English-EWT/Data/UD_English-EWT/en_ewt-ud-test.conllu"
+    conll_file = os.path.abspath(r'C:\Users\tobia\Documents\UTwente\ITECH\Natural Language Processing\Project\SentenceGenerator\Data\UD_English-EWT\en_ewt-ud-test.conllu')
     file = "hp.txt"
-    
+
     model = Predicter(conll_file)  # Predicter("hp.txt", stem=False)
     feature = ("it", None)
     
